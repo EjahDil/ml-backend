5062a57f47394fcd294d4edb1a74de7c901a8a83
Mianja-Tiana <arabemanantsoa@aimsammi.org>
2025-10-31 20:29:17 +0000

    add user model

---
+++ src/models/users.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
+import uuid
+
+class UserBase(BaseModel):
+    full_name: Optional[str] = None
+    phone: str
+    team: str
+    role: str
+    address: str
+    password: str
+    created_at: date
+    updated_at: date
+    is_active: bool
+
+    class Config:
+        from_attributes = True
+
+class UserCreate(UserBase):
+    password: str
+
+    class Config:
+        from_attributes = True
+    
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserResponse(UserBase):
+    id: uuid.UUID
+    created_at: date
+    is_active: bool
+
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
