06200c729dfcef51a52b4388ef1a28ba2836badc
ejahdilan <dilan.ejah@aims-cameroon.org>
2025-11-07 09:37:54 +0000

    Fix: Enabled model loading

--- src/utils/ml_utils.py
+++ src/utils/ml_utils.py
@@ -10,31 +10,11 @@ from mlflow.tracking import MlflowClient
 load_dotenv()
 
 # Absolute path to your mlruns folder
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
-MLFLOW_TRACKING_URI = f"file://{os.path.join(BASE_DIR, 'mlruns')}"
-
-MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
-
-mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-
-# Absolute path to your desired mlruns folder
-BASE_DIR = "/Users/ammi/Documents/telCustomerChurn"
-MLRUNS_PATH = os.path.join(BASE_DIR, "mlruns")
-
-# Make sure the directory exists
-os.makedirs(MLRUNS_PATH, exist_ok=True)
-
-# Set MLflow tracking URI
-MLFLOW_TRACKING_URI = f"{MLRUNS_PATH}"
-
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
 
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 MODEL_NAME = os.getenv("MODEL_NAME", "Churn_RandomForest")
-
-# MODEL_VERSION = os.getenv("MODEL_VERSION", "1")  # specify a version explicitly
 
 # Get the latest version of the model from MLflow
 client = MlflowClient()
@@ -54,7 +34,6 @@ print(f"Using model URI: {model_uri}")
 # # Load the model using the sklearn flavor
 model = mlflow.sklearn.load_model(model_uri)
 
-
 client = MlflowClient()
 
 # Get all versions of the model
@@ -66,7 +45,6 @@ run_id = latest_version_info.run_id
 
 # Optionally, print vers1:5000 (Press CTRL+C to quit)ion and stage
 print(f"Latest version: {latest_version_info.version}, stage: {latest_version_info.current_stage}")
-
 
 # Get experiment name from environment or use default
 PREPROCESS_EXPERIMENT_NAME = os.getenv("EXPERIMENT_NAME", "telecom_churn_preprocessing")
