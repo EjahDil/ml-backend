1c79fd422e51849bcb1b2226d9c6b54fc2319a1b
ejahdilan <dilan.ejah@aims-cameroon.org>
2025-11-10 21:21:14 +0000

    Fix:Solved endpoint issues

--- src/schemas/schema.py
+++ src/schemas/schema.py
@@ -19,15 +19,16 @@ class User(UserBase):
 class UserInDB(User):
     hashed_password: str
 
+
 class UserOut(BaseModel):
-    id: int
+    id: UUID
     username: str
-    email: str
-    created_at: Optional[str]
+    full_name: str | None = None
+    role: str
+    created_at: datetime
 
     class Config:
-       from_attributes= True 
-
+        from_attributes= True 
 
 # Token schemas
 
